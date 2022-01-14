eded83ccb81a3de4e83717eaff2e856b8ddf18dd
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-01-14 18:17:24 +0200

    create-react-app: basic import and asset import

--- packages/create-react-app/src/Checklist/Checklist.tsx
+++ packages/create-react-app/src/Checklist/Checklist.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Button } from 'ui-library';
 import './Checklist.css';
+
+import mountainPhoto from 'ui-library/assets/mountain.jpg';
 
 function Checklist() {
   return (
@@ -9,7 +12,13 @@ function Checklist() {
         <li>
           <h2> Basic component import from "ui-library"</h2>
           <p> If the below button is correctly styled, the import has worked.</p>
+          <Button variant='success' loading> Green Loading Button</Button>
+        </li>
 
+        <li>
+          <h2> Asset import from 'ui-library'</h2>
+          <p> If the picture below correctly shos, the import has worked.</p>
+          <img src={mountainPhoto} width="300" alt="Mountain Photo" />
         </li>
       </ol>
     </section>
