b8f5df7d5c408ef15f9366e17b7d6a39cd45707e
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-01-14 16:20:02 +0200

    create-react-app: create base Checklist page

---
+++ packages/create-react-app/src/Checklist/Checklist.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Checklist.css';
+
+function Checklist() {
+  return (
+    <section className="Checklist">
+      <h1> Lerna Playground | create-react-app</h1>
+      <ol>
+        <li>
+          <h2> Basic component import from "ui-library"</h2>
+          <p> If the below button is correctly styled, the import has worked.</p>
+
+        </li>
+      </ol>
+    </section>
+  );
+}
+
+export default Checklist;
